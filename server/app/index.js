711d95f8b205726e06dc77c61d2b35fe99e3c981
Joao Brilhante <joao.brilhante@outlook.com>
2020-03-29 01:07:05 +0100

    Create the server.

--- server/app/index.js
+++ server/app/index.js
@@ -1,4 +1,27 @@
-const buildServer = require('./build-server.js')
-const logger = require('./utils/logger.js')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const notFoundHandler = require('./utils/handlers/not-found-handler');
+const errorHandler = require('./utils/handlers/error-handler');
 
-buildServer((server) => logger.info(`Server is listening on port ${server.address().port}`))
+// Configure the middleware.
+const app = express();
+const logger = morgan('dev');
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Configure the application.
+app.disable('x-powered-by');
+app.use(cors());
+app.use(jsonParser);
+app.use(urlencodedParser);
+app.use(logger);
+app.use(routes);
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+// Start the application.
+app.listen(process.env.PORT || 3000);
