c00295a71637ce1cd8dcaf78c185a1387623a511
Joao Brilhante <joao.brilhante@outlook.com>
2020-03-29 01:04:39 +0100

    Add initial Sequelize config.

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,11 +1,39 @@
-const Answer = require('./answer.model.js')
-const Question = require('./question.model.js')
-const Quiz = require('./quiz.model.js')
-const User = require('./user.model.js')
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const config = require('../config/sequelize.js');
 
-module.exports = {
-  Answer,
-  Question,
-  Quiz,
-  User,
-}
+const db = {};
+
+// Initialize the sequelize instance.
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+// Import the sequelize models.
+fs
+  .readdirSync(__dirname)
+  .filter((file) => (
+    (file.indexOf('.') !== 0)
+    && (file !== path.basename(__filename))
+    && (file.slice(-3) === '.js')
+  ))
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+// Run the models associations.
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+// Export the database objects.
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+module.exports = db;
