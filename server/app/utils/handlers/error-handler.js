19a2532288634b3256e7d087a6604ae03ec47689
Joao Brilhante <joao.brilhante@outlook.com>
2020-03-30 23:20:18 +0200

    Fix the error handler.

--- server/app/utils/handlers/error-handler.js
+++ server/app/utils/handlers/error-handler.js
@@ -1,7 +1,9 @@
-// eslint-disable-next-line no-unused-vars
 const errorHandler = (error, req, res, next) => {
-  res.status(error.code || 500);
-  res.json({ error });
+  // TODO: Secure for production.
+  res
+    .status(error.code || 500)
+    .json({ error })
+    .end();
 };
 
 module.exports = errorHandler;
