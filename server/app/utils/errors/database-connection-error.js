4bf27ba234f60f009b959382683b099868459843
Joao Brilhante <joao.brilhante@outlook.com>
2020-03-30 23:20:05 +0200

    Remove unused custom errors (let's use the sequelize ones).

--- server/app/utils/errors/database-connection-error.js
+++
@@ -1,9 +0,0 @@
-const ApplicationError = require('./application-error');
-
-class DatabaseConnectionError extends ApplicationError {
-  constructor() {
-    super(500, 'The database could not be accessed.');
-  }
-}
-
-module.exports = DatabaseConnectionError;
