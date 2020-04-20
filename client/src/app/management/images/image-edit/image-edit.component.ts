825a08ef9a059e1f9f8f0f68cdaafb6d8a436e6b
Martin <martin.bouteiller@etu.unice.fr>
2020-04-20 15:23:03 +0200

    Ajout de la liste des images

---
+++ client/src/app/management/images/image-edit/image-edit.component.ts
@@ -0,0 +1,55 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {faUserEdit, faUserPlus} from '@fortawesome/free-solid-svg-icons';
+import {Image} from "../../../../models/image.model";
+import {ImageService} from "../../../../services/image.service";
+
+@Component({
+  selector: 'app-imageEdit',
+  templateUrl: './image-edit.component.html',
+  styleUrls: ['./image-edit.component.scss'],
+})
+
+export class ImageEditComponent implements OnInit {
+
+  imageToEdit: Image;
+  accessibilities: String[] = ['none', 'TBD'];
+  icon = faUserPlus;
+  buttonText: String;
+  @ViewChild('name') nameInput: ElementRef;
+  @ViewChild('path') pathInput: ElementRef;
+  @ViewChild('keywords') keywordsInput: ElementRef;
+
+  constructor(private imageService: ImageService, private router: Router, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.imageToEdit = this.imageService.getImageToModify();
+    if (this.imageToEdit === undefined) {
+      this.router.navigate(['../'], {relativeTo: this.route});
+    } else {
+      if (this.imageToEdit == null) {
+        this.buttonText = 'Ajouter';
+      } else {
+        this.icon = faUserEdit;
+        this.buttonText = 'Modifier';
+      }
+    }
+  }
+
+  createModifyImage() {
+    let imageToCreate: Image = {
+      id: -1,
+      name: this.nameInput.nativeElement.value,
+      path: this.pathInput.nativeElement.value,
+      keywords: this.keywordsInput.nativeElement.value,
+    };
+    if (this.imageToEdit == null) {
+      this.imageService.createImage(imageToCreate);
+    } else {
+      imageToCreate.id = this.imageToEdit.id;
+      this.imageService.updateImage(imageToCreate);
+    }
+  }
+
+}
