802fbd4953334d6ff6008a5fcfcb2b917062ccb0
Joao Brilhante <joao.brilhante@outlook.com>
2020-05-08 20:24:35 +0200

    Make Docker work for Windows Home.

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-echo 'Downloading client dependencies...'
-cd client
-npm install
+cat << "EOF"
+  _____          _                      _   _
+ |  __ \        | |                    | | (_)
+ | |__) |___  __| | ___ _ __ ___  _ __ | |_ _  ___  _ __
+ |  _  // _ \/ _` |/ _ \ '_ ` _ \| '_ \| __| |/ _ \| '_ \
+ | | \ \  __/ (_| |  __/ | | | | | |_) | |_| | (_) | | | |
+ |_|  \_\___|\__,_|\___|_| |_| |_| .__/ \__|_|\___/|_| |_|
+                                 | |
+                                 |_|
 
-echo 'Downloading server dependencies...'
-cd ../server
-npm install
+EOF
 
-echo 'Starting the project...'
-cd ..
-sudo docker-compose up --build
+echo "Building the project"
+export COMPOSE_CONVERT_WINDOWS_PATHS=1
+docker-compose up --build
