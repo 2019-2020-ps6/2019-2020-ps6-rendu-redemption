f50bcddd7817c17ac545becaa8e8d53258492821
Joao Brilhante <joao.brilhante@outlook.com>
2020-05-08 04:19:54 +0200

    Make the whole project use Docker and Docker Compose (needs testing).

---
+++ server/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo 'Hello, world!';
+
+# Wait for database to be ready.
+until nc -z -v -w60 postgres 5432
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
+# Run migrations and seeds.
+npx sequelize db:migrate
+npx sequelize db:seed:all
+
+# Run the server.
+npm run dev
