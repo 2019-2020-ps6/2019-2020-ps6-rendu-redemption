29a9446c3f3011c2a749736caaa7b40c88d5bb8e
Martin <martin.bouteiller@etu.unice.fr>
2020-05-17 12:11:04 +0200

    Changement du bouton retour au menu et début de la gestion de la size du container en fonction du profil

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, DoCheck, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Guest} from "../models/guest.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  styles: [`
+    .size {
+      max-width: 2000px;
+    }`
+    ]
 })
-export class AppComponent {
+export class AppComponent implements DoCheck{
   title = 'PS6 Redemption';
+
+  public guest: Guest;
+  public profile: string;
+
+  ngDoCheck() {
+
+    this.guest = JSON.parse(sessionStorage.getItem('selectedGuest'));
+    if (this.guest == null) {
+      this.profile = 'none';
+    }
+    if (this.guest != null) {
+      this.profile = this.guest.accessibility;
+    }
+  }
+
+  setSize() {
+    let style = {
+      size: this.profile === 'tbd1'
+    };
+
+    return style;
+  }
 }
 
